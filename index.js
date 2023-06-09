f62c9a44652a291887490fe7844975f63b439bf9
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-06-09 16:45:45 +0600

    make admin done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ev6cf0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,20 +59,19 @@ async function run() {
     });
 
     // make user admin api
-    // app.patch('/users/admin/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log(id);
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: 'admin'
-    //     },
-    //   };
 
-    //   const result = await usersCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
 
-    // })
+    })
 
 
 
